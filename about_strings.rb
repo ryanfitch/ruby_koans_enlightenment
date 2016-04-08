046979ccac6243e21ea4adc4e423a03a1915eac3
Ryan Fitch <ryanfitch@mac.com>
2016-04-08 16:00:51 -0700

    seeking enlightenment

--- about_strings.rb
+++ about_strings.rb
@@ -80,15 +80,15 @@ EOS
     hi = original_string
     there = "World"
     hi += there
-    assert_equal __, original_string
+    assert_equal "Hello, ", original_string
   end
 
   def test_the_shovel_operator_will_also_append_content_to_a_string
     hi = "Hello, "
     there = "World"
     hi << there
-    assert_equal __, hi
-    assert_equal __, there
+    assert_equal "Hello, World", hi
+    assert_equal "World", there
   end
 
   def test_the_shovel_operator_modifies_the_original_string
